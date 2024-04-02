32af7cfd8b0229e7f33cad0a6d93f98ecd741856
Sir.Eti <55893929+Etiabasi@users.noreply.github.com>
2024-04-02 20:19:29 -0230

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -18,7 +18,8 @@ const links = [
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Users', href: '/dashboard/users', icon: UserGroupIcon },
+  //{ name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
